c3022a6b505dbd927c3b798a5ce8810eb8712dd5
fosterjok <fosterjok@mail.ru>
2022-10-21 23:37:59 +0300

    47

---
+++ 47/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задайте двумерный массив размером m×n, заполненный случайными вещественными числами
+int m=3, n= 4;
+double [,] array = new double [m,n];
+int s = 10;
+for (int i = 0; i < m; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        array[i,j]= new Random().Next(-90,100)/(double)s;
+        Console.Write($"{array[i,j]} ");
+    } Console.WriteLine();
+}
